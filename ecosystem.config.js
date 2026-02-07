a3125624141fde4dd1ef71455ce80a60104138c5
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2026-02-07 14:02:01 +0600

    ecosystem.config added

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'RoznaComarker_Backend',
+      script: './src/server.js',
+      cwd: '/home/comarkerback/htdocs/comarkerback.roznahub.com/RoznaComarker_Backend1',
+      watch: false,
+      env_file: '.env'  
+    }
+  ]
+};
