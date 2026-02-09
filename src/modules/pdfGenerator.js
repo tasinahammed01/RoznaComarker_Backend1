217196bff90d660030a54c170dd9e268a1621874
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2026-02-09 13:06:47 +0600

    pdf issure checking

--- src/modules/pdfGenerator.js
+++ src/modules/pdfGenerator.js
@@ -766,8 +766,13 @@ async function renderSubmissionPdf({ header, imageUrl, transcriptText, issues, f
   });
 
   const browser = await puppeteer.launch({
-    headless: 'new',
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu'
+    ]
   });
 
   try {
