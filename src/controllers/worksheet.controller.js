1aff775d3ef6563b8b118f8e924f4ee16e29f865
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2026-06-11 21:12:15 +0600

    url changed

--- src/controllers/worksheet.controller.js
+++ src/controllers/worksheet.controller.js
@@ -1996,7 +1996,7 @@ async function shareWorksheet(req, res) {
       }
     }
 
-    const shareUrl = `${process.env.FRONTEND_URL || "http://localhost:4200"}/shared/worksheets/${worksheet.shareToken}`;
+    const shareUrl = `${process.env.FRONTEND_URL || "http://82.112.234.151:4200"}/shared/worksheets/${worksheet.shareToken}`;
 
     return res.json({
       success: true,
